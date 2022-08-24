75ea7e304242a5cd764b90c4e5c70c92325d7241
JuanMaOrtizE <53711761+JuanMaOrtizE@users.noreply.github.com>
2022-08-24 18:54:01 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -5,9 +5,96 @@ public class Control {
 
 	public void enlazar(TV tv) {
 		this.tv = tv;
-		tv.control=Control.this;
+		this.tv.control=this;
 		
 		
+		
+	}
+	
+	
+	public void turnOff(){
+		if (tv.estado==true){
+			tv.estado= false;
+			
+			
+		}
+		else {
+			tv.estado= true;
+		}
+		
+		
+	}
+	
+	public void turnOn(){
+		if (tv.estado==false){
+			tv.estado= true;
+			
+			
+		}
+		else {
+			tv.estado= false;
+		}
+		
+		
+	}
+	
+	
+	public void canalUp() {
+		if (tv.estado==true){
+			if(tv.canal>=1 && tv.canal <120) {
+				tv.canal++;
+				
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	public void canalDown() {
+		if (tv.estado==true){
+			if(tv.canal>1 && tv.canal <=120) {
+				tv.canal--;
+				
+			}
+			
+			
+		}
+		
+		
+		
+	}
+	
+	public void volumenUp(){
+		if (tv.estado==true){
+			if(tv.volumen>=0 && tv.volumen <7) {
+				tv.volumen++;
+				
+			}
+			
+			
+		}
+		
+		
+	}
+	
+	public void volumenDown(){
+		if (tv.estado==true){
+			if(tv.volumen>0 && tv.volumen <=7) {
+				tv.volumen--;
+				
+			}
+			
+			
+		}
+		
+		
+	}
+	public void setCanal(int canal) {
+		if (canal>=1 && canal <=120) {
+			this.tv.canal = canal;
+		}
 		
 	}
 
